fd4d780bb57db05b6468f90f62517716b920e52a
Miguel Brás <miguelbrasneves@gmail.com>
2019-04-26 10:04:52 +0100

    Updated Constant.java of client
    Constant update to include hostname and ports

--- Assignment 2/manager_machine/repairshop_sd/repairshop_sd/src/utils/Constants.java
+++ Assignment 2/manager_machine/repairshop_sd/repairshop_sd/src/utils/Constants.java
@@ -29,61 +29,61 @@ public class Constants {
     /**
      * Terminal do Servidor do Repository
      */
-    public static final String REPAIR_SERVER_HOST = "localhost";
+    public static final String REPAIR_SERVER_HOST = "l040101-ws06.ua.pt";
 
     /**
      * Porta do Servidor da Repair Area
      */
-    public static final int REPAIR_AREA_SERVER_PORT_NUMBER = 1234;
+    public static final int REPAIR_AREA_SERVER_PORT_NUMBER = 22105;
 
     /**
      * Terminal do Servidor do Repository
      */
-    public static final String REPOSITORY_SERVER_HOST = "localhost";
+    public static final String REPOSITORY_SERVER_HOST = "l040101-ws01.ua.pt";
 
     /**
      * Porta do Servidor do Repository
      */
-    public static final int REPOSITORY_SERVER_PORT = 1234;
+    public static final int REPOSITORY_SERVER_PORT = 22100;
 
     /**
      * Terminal do Servidor do Lounge
      */
-    public static final String LOUNGE_SERVER_HOST = "localhost";
+    public static final String LOUNGE_SERVER_HOST = "l040101-ws02.ua.pt";
 
     /**
      * Porta do Servidor do Lounge
      */
-    public static final int LOUNGE_SERVER_PORT = 1234;
+    public static final int LOUNGE_SERVER_PORT = 22101;
 
     /**
      * Terminal do Servidor do Park
      */
-    public static final String PARK_SERVER_HOST = "localhost";
+    public static final String PARK_SERVER_HOST = "l040101-ws04.ua.pt";
 
     /**
      * Porta do Servidor do Park
      */
-    public static final int PARK_SERVER_PORT = 1234;
+    public static final int PARK_SERVER_PORT = 22103;
 
     /**
      * Terminal do Servidor do Outside World
      */
-    public static final String OUTSIDE_WORLD_HOST = "localhost";
+    public static final String OUTSIDE_WORLD_HOST = "l040101-ws03.ua.pt";
 
     /**
      * Porta do Servidor do Outside World
      */
-    public static final int OUTSIDE_WORLD_PORT = 1234;
+    public static final int OUTSIDE_WORLD_PORT = 22102;
 
     /**
-     * Terminal do Servidor do Outside World
+     * Terminal do Servidor do Supplier Site
      */
-    public static final String SUPPLIER_SITE_HOST= "localhost";
+    public static final String SUPPLIER_SITE_HOST= "l040101-ws05.ua.pt";
 
     /**
-     * Porta do Servidor do Outside World
+     * Porta do Servidor do Supplier Site
      */
-    public static final int SUPPLIER_SITE_PORT = 1234;
+    public static final int SUPPLIER_SITE_PORT = 22104;
 }
 
