7401b38bc8cbf960af6e8069ddfc8cde62bca26a
Cristiano Vagos <cristianovagos@ua.pt>
2019-04-28 12:32:43 +0100

    added cd command for each machine deploy script

--- Assignment 2/deploy.sh
+++ Assignment 2/deploy.sh
@@ -23,12 +23,12 @@ ansible-playbook playbook.yml
 printf "Launching a terminal window from each machine...\n"
 
 # open each terminal
-xterm -e bash -c 'ssh sd0101@l040101-ws01.ua.pt;' &
-xterm -e bash -c 'ssh sd0101@l040101-ws02.ua.pt;' &
-xterm -e bash -c 'ssh sd0101@l040101-ws03.ua.pt;' &
-xterm -e bash -c 'ssh sd0101@l040101-ws04.ua.pt;' &
-xterm -e bash -c 'ssh sd0101@l040101-ws05.ua.pt;' &
-xterm -e bash -c 'ssh sd0101@l040101-ws06.ua.pt;' &
-xterm -e bash -c 'ssh sd0101@l040101-ws07.ua.pt;' &
-xterm -e bash -c 'ssh sd0101@l040101-ws08.ua.pt;' &
-xterm -e bash -c 'ssh sd0101@l040101-ws09.ua.pt;' 
+xterm -e bash -c "ssh -t sd0101@l040101-ws01.ua.pt 'cd repository/; bash';" &
+xterm -e bash -c "ssh -t sd0101@l040101-ws02.ua.pt 'cd lounge/; bash';" &
+xterm -e bash -c "ssh -t sd0101@l040101-ws03.ua.pt 'cd outsideworld/; bash';" &
+xterm -e bash -c "ssh -t sd0101@l040101-ws04.ua.pt 'cd park/; bash';" &
+xterm -e bash -c "ssh -t sd0101@l040101-ws05.ua.pt 'cd suppliersite/; bash';" &
+xterm -e bash -c "ssh -t sd0101@l040101-ws06.ua.pt 'cd repairarea/; bash';" &
+xterm -e bash -c "ssh -t sd0101@l040101-ws07.ua.pt 'cd customer/; bash';" &
+xterm -e bash -c "ssh -t sd0101@l040101-ws08.ua.pt 'cd mechanic/; bash';" &
+xterm -e bash -c "ssh -t sd0101@l040101-ws09.ua.pt 'cd manager/; bash';"
